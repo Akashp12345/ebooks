6faf9e8652b384e1167c3d0300d2e39eae6bda72
Akashp12345 <akash.p6201@gmail.com>
2024-06-18 17:20:56 +0530

    changed appspec.yml script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables
+APP_DIR="/home/ubuntu/ebooks"
+BRANCH="main"
+
+# Navigate to the app directory
+cd $APP_DIR
+
+# Pull the latest changes from the repository
+git pull origin $BRANCH
+
+# Install dependencies
+sudo service nginx restartgit 
